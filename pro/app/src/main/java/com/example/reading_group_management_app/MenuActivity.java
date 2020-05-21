54f87de632baacc26dc2b27bda500a94aff365e9
jiminAn <als398@naver.com>
2020-05-21 11:19:42 +0900

    메인페이지 MenuActivity로 수정

---
+++ pro/app/src/main/java/com/example/reading_group_management_app/MenuActivity.java
@@ -0,0 +1,47 @@
+package com.example.reading_group_management_app;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button loginBtn = (Button)findViewById(R.id.login_btn);
+        Button mainpageBtn = (Button)findViewById(R.id.mainpage_btn);
+        Button listSettingBtn = (Button)findViewById(R.id.list_setting_btn);
+
+        mainpageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), MainPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        listSettingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), first_list_setting_popup.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
